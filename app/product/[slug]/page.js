77a84ccd2fb604a038cd23ad1ae11716b583ee3f
Genie-Usman <musman7533@gmail.com>
2025-02-02 15:22:32 +0500

    Creating pages for the website

---
+++ app/product/[slug]/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Product = ({ params }) => {
+    const { slug } = params;
+
+    return (
+        <div>
+            <h1 className='bg-slate-500'>Product: {slug}</h1>
+        </div>
+    );
+}
+
+
+export default Product

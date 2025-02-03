e9296803556ed566b9662cdf98812df9e657370d
Genie-Usman <musman7533@gmail.com>
2025-02-03 17:39:09 +0500

    Adding SideCart State

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,35 +1,49 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { FaShoppingCart } from "react-icons/fa";
+import SideCart from './SideCart';
 
 const Navbar = () => {
+    const [cartOpen, setCartOpen] = useState(false);
+
+    const toggleCart = () => {
+        setCartOpen((prev) => !prev);
+    };
+
     return (
         <div>
             <header className="text-gray-600 body-font">
                 <div className="container mx-auto flex flex-wrap py-2 md:py-1 flex-col md:flex-row items-center shadow-md">
                     <Link href={'/'}>
-                    <Image
-                        className='m-auto md:ml-5'
-                        src="/logo-banner.png"
-                        alt="logo" width={200}
-                        height={20}
-                        priority={true} 
-                        style={{ height: "auto" }}
-                        loading="eager"
+                        <Image
+                            className='m-auto md:ml-5'
+                            src="/logo-banner.png"
+                            alt="logo" 
+                            width={200}
+                            height={20}
+                            priority={true} 
+                            style={{ height: "auto" }}
+                            loading="eager"
                         />
-                        </Link>
+                    </Link>
                     <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-                        <Link href={'/tshirts'} className="mr-5 text-xs md:text-base font-bold hover:text-gray-900">Tshirts</Link>
-                        <Link href={'/hoodies'} className="mr-5 text-xs md:text-base font-bold hover:text-gray-900">Hoodies</Link>
-                        <Link href={'/stickers'} className="mr-5 text-xs md:text-base font-bold hover:text-gray-900">Stickers</Link>
-                        <Link href={'/mugs'} className="mr-5 text-xs md:text-base font-bold hover:text-gray-900">Mugs</Link>
-                    <FaShoppingCart className='text-base md:text-2xl absolute right-2 top-5 md:my-auto'/>
+                        <Link href={'/tshirts'} className="mr-5 text-xs md:text-base font-bold hover:text-devstyle">Tshirts</Link>
+                        <Link href={'/hoodies'} className="mr-5 text-xs md:text-base font-bold hover:text-devstyle">Hoodies</Link>
+                        <Link href={'/stickers'} className="mr-5 text-xs md:text-base font-bold hover:text-devstyle">Stickers</Link>
+                        <Link href={'/mugs'} className="mr-5 text-xs md:text-base font-bold hover:text-devstyle">Mugs</Link>
+                        <FaShoppingCart 
+                            onClick={toggleCart} 
+                            className='text-base text-devstyle md:text-2xl absolute right-2 top-5 md:my-auto cursor-pointer' 
+                        />
                     </nav>
+                    <SideCart cartOpen={cartOpen} toggleCart={toggleCart} />
                 </div>
             </header>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
---
+++ components/SideCart.js
@@ -0,0 +1,34 @@
+"use client";
+
+import React from 'react';
+import { AiFillCloseCircle, AiFillPlusCircle, AiFillMinusCircle } from "react-icons/ai";
+import { RiShoppingBag4Fill } from "react-icons/ri";
+
+const SideCart = ({ cartOpen, toggleCart }) => {
+    return (
+        <div
+            className={`sideCart bg-pink-200 w-72 absolute top-1 right-0 h-full px-5 py-10 transform transition-transform rounded ${cartOpen ? 'translate-x-0' : 'translate-x-full '
+                }`}
+        >
+            <h2 className='font-bold text-xl text-center '>Shopping Cart</h2>
+            <span onClick={toggleCart} className='absolute top-5 right-2 cursor-pointer'>
+                <AiFillCloseCircle className='hover:text-blue-400 text-xl md:text-2xl text-devstyle' />
+            </span>
+            <ol className='mx-1 font-semibold list-decimal'>
+                <li>
+                    <div className='item flex my-5'>
+                        <div className='w-2/3 text-base font-semibold'>T-shirt - Code Your Look</div>
+                        <div className='w-1/3 font-semibold flex justify-center items-center'>
+                            <AiFillMinusCircle className='text-devstyle text-lg cursor-pointer' />
+                            <span className='mx-2'>1</span>
+                            <AiFillPlusCircle className='text-devstyle text-lg cursor-pointer' />
+                        </div>
+                    </div>
+                </li>
+            </ol>
+            <button className="flex ml-2 text-white bg-devstyle border-0 py-2 px-4 focus:outline-none hover:bg-blue-400 rounded"><RiShoppingBag4Fill className='m-1' />Checkout</button>
+        </div>
+    );
+};
+
+export default SideCart;

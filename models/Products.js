382af919ada8f3b2cc2ddf122e2aed5a6d4ed7e6
Genie-Usman <musman7533@gmail.com>
2025-02-06 23:57:05 +0500

    Product Schema Created

---
+++ models/Products.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
+    size: { type: String},
+    color: { type: String},
+    price: { type: Number, required: true},
+    availableQuantity: { type: Number, required: true},
+
+}, {timestamps: true});
+
+export default mongoose.model('Products', ProductSchema);

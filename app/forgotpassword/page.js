34ec52a14e4732be928c204ae8e65d2a45172994
Genie-Usman <musman7533@gmail.com>
2025-02-06 23:23:58 +0500

    Forgot Password Page Created

--- app/forgotpassword/page.js
+++ app/forgotpassword/page.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const ForgotPassword = () => {
   return (
-    <div>
-      
+    <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      <div className="w-full max-w-md bg-white p-8 rounded-2xl shadow-lg">
+        <div className="flex justify-center">
+          <Image
+            className="m-auto"
+            src="/logo.png"
+            alt="logo"
+            width={500}
+            height={250}
+            style={{ width: "22%", height: "auto" }}
+          />
+        </div>
+        <h2 className="text-center text-2xl font-bold mt-2">
+          Forgot Password
+        </h2>
+        <p className="text-center text-gray-600">
+            Or 
+            <Link href={"/login"} className="ml-2 text-devstyle font-bold text-sm hover:text-red-700">Login</Link>
+        </p>
+        <form className="mt-6">
+          <div>
+            <input
+              type="email"
+              className="w-full px-4 py-2 mt-2 border-0 rounded-md focus:ring-1 focus:outline-none focus:ring-devstyle"
+              placeholder="Email address"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full mt-4 bg-devstyle text-white py-2 rounded-md hover:bg-red-700 transition"
+          >
+            Continue
+          </button>
+        </form>
+      </div>
     </div>
   )
 }

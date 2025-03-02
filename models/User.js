63d4f0d0df1e4443fc051c2013391642455e4644
Genie-Usman <Genie-Usman@users.noreply.github.com>
2025-03-02 22:26:26 +0500

    User Model Updated

--- models/User.js
+++ models/User.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    phone: { type: String, required: true, unique: true },
+    phone: { type: String, unique: true },
     password: { type: String, required: true },
-    address: { type: String, required: true },
-    zipcode: { type: Number, required: true },
+    address: { type: String },
+    zipcode: { type: Number },
 
 }, {timestamps: true});
 
